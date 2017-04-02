b5bd7ee17c0b9a9160c6ba1584005154b97eac89
kevonthebus <kmatth.007@gmail.com>
2017-04-02 10:02:22 -0400

    first branch

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017'
+    }
+  }
+render() {
+  return(
+<div className="App">
+  <div className="App-title"> Countdown to {} </div>
+  <div>
+    <div className="Clock-days"> 14 Days</div>
+    <div className= "Clock-hours"> 30 Hours </div>
+    <div className="Clock-minutes"> 15 minutes </div>
+    <div className="Clock-seconds">20 seconds </div>
+  </div>
+  <div>
+  <input placeholder='new date' />
+  <button>Submit</button>
+  </div>
+  </div>
+)
+  }
+}
+export default App;
